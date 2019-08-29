ef8cfeae2aab2b349ac6d2581c38cba09fdd8460
Jerôme Landolt <jl@nexplore.ch>
2019-08-29 13:13:52 +0200

    initial commit

---
+++ M120Projekt/APIDemo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace M120Projekt
+{
+    static class APIDemo
+    {
+        #region KlasseA
+        // Create
+        public static void DemoACreate()
+        {
+            Debug.Print("--- DemoACreate ---");
+            // KlasseA
+            Data.Produkt produkt1 = new Data.Produkt();
+            produkt1.ProduktName = "Artikel 13";
+            produkt1.Erscheinungsdatum = DateTime.Today;
+            produkt1.AnLager = true;
+            produkt1.Preis = 21;
+            Int64 produkt1ID = produkt1.Erstellen();
+            Debug.Print("Artikel erstellt mit Id:" + produkt1ID);
+        }
+        public static void DemoACreateKurz()
+        {
+            Data.Produkt produkt2 = new Data.Produkt { ProduktName = "Artikel 2", AnLager = true, Erscheinungsdatum = DateTime.Today, Preis = 21};
+            Int64 produkt2ID = produkt2.Erstellen();
+            Debug.Print("Artikel erstellt mit Id:" + produkt2ID);
+        }
+
+        // Read
+        public static void DemoARead()
+        {
+            Debug.Print("--- DemoARead ---");
+            // Demo liest alle
+            foreach (Data.Produkt klasseA in Data.Produkt.LesenAlle())
+            {
+                Debug.Print("Artikel Id:" + klasseA.ProduktID + " Name:" + klasseA.ProduktName);
+            }
+        }
+        // Update
+        public static void DemoAUpdate()
+        {
+            Debug.Print("--- DemoAUpdate ---");
+            // KlasseA ändert Attribute
+            Data.Produkt produkt1 = Data.Produkt.LesenID(1);
+            produkt1.ProduktName = "Artikel 1 nach Update";
+            produkt1.Aktualisieren();
+        }
+        // Delete
+        public static void DemoADelete()
+        {
+            Debug.Print("--- DemoADelete ---");
+            //Data.Produkt.LesenID(2).Loeschen();
+            Debug.Print("Artikel mit Id 2 gelöscht");
+        }
+        #endregion
+    }
+}
---
+++ M120Projekt/Data/Produkt.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace M120Projekt.Data
+{
+    public class Produkt
+    {
+        #region Datenbankschicht
+        [Key]
+        public Int64 ProduktID { get; set; }
+        [Required]
+        public String ProduktName { get; set; }
+        [Required]
+        public DateTime Erscheinungsdatum { get; set; }
+        [Required]
+        public Boolean AnLager { get; set; }
+        [Required]
+        public Decimal Preis { get; set; }
+        #endregion
+        #region Applikationsschicht
+        public Produkt() { }
+        [NotMapped]
+        public String BerechnetesAttribut
+        {
+            get
+            {
+                return "Im Getter kann Code eingefügt werden für berechnete Attribute";
+            }
+        }
+        public static List<Produkt> LesenAlle()
+        {
+            using (var db = new Context())
+            {
+                return (from record in db.KlasseA select record).ToList();
+            }
+        }
+        public static Produkt LesenID(Int64 klasseAId)
+        {
+            using (var db = new Context())
+            {
+                return (from record in db.KlasseA where record.ProduktID == klasseAId select record).FirstOrDefault();
+            }
+        }
+        public static List<Produkt> LesenAttributGleich(String suchbegriff)
+        {
+            using (var db = new Context())
+            {
+                return (from record in db.KlasseA where record.ProduktName == suchbegriff select record).ToList();
+            }
+        }
+        public static List<Produkt> LesenAttributWie(String suchbegriff)
+        {
+            using (var db = new Context())
+            {
+                return (from record in db.KlasseA where record.ProduktName.Contains(suchbegriff) select record).ToList();
+            }
+        }
+        public Int64 Erstellen()
+        {
+            if (this.ProduktName == null || this.ProduktName == "") this.ProduktName = "leer";
+            if (this.Erscheinungsdatum == null) this.Erscheinungsdatum = DateTime.MinValue;
+            using (var db = new Context())
+            {
+                db.KlasseA.Add(this);
+                db.SaveChanges();
+                return this.ProduktID;
+            }
+        }
+        public Int64 Aktualisieren()
+        {
+            using (var db = new Context())
+            {
+                db.Entry(this).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return this.ProduktID;
+            }
+        }
+        public void Loeschen()
+        {
+            using (var db = new Context())
+            {
+                db.Entry(this).State = System.Data.Entity.EntityState.Deleted;
+                db.SaveChanges();
+            }
+        }
+        public override string ToString()
+        {
+            return ProduktID.ToString(); // Für bessere Coded UI Test Erkennung
+        }
+        #endregion
+    }
+}
